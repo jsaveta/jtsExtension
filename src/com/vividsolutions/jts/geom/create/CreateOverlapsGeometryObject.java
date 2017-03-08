0731d243a4c3e07106c2d5dd1e95623b3de7c72d
jsaveta <jsaveta@gmail.com>
2017-03-08 15:28:05 +0200

    overlaps relation

--- src/com/vividsolutions/jts/geom/create/CreateOverlapsGeometryObject.java
+++ src/com/vividsolutions/jts/geom/create/CreateOverlapsGeometryObject.java
@@ -5,6 +5,8 @@
  */
 package com.vividsolutions.jts.geom.create;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
@@ -14,6 +16,9 @@ import com.vividsolutions.jts.geom.MultiPoint;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.gml2.LineStringGenerator;
+import com.vividsolutions.jts.io.gml2.PointGenerator;
+import java.util.Random;
 
 /**
  *
@@ -36,7 +41,7 @@ public class CreateOverlapsGeometryObject extends GeometryType {
     public Geometry generateGeometry() {
         String givenGeometryType = this.given.getGeometryType();
         GeometryFactory geometryFactory = new GeometryFactory();
-        this.returned = this.given; //in case that there is nothing to return
+//        this.returned = this.given; //in case that there is nothing to return
 
         switch (givenGeometryType) {
             case "Point":
@@ -95,8 +100,66 @@ public class CreateOverlapsGeometryObject extends GeometryType {
                     case "MultiPoint":
                         break;
                     case "LineString":
-                        //@TODO: IMPLEMENT THIS FIRST
+                        Random randomGenerator = new Random();
+                        Envelope env = lineString.getEnvelopeInternal();
+
+                        int chunk = randomGenerator.nextInt(lineString.getCoordinates().length - 2) + 2;
+                        LineString[] lineArray = getLineStringArray(lineString, chunk);
+
+                        int randomInt = randomGenerator.nextInt(2); //keep first or last part of the line
+                        LineString line = lineArray[randomInt];
+
+                        int length = lineString.getCoordinates().length;
+                        int poinsToIntersect = line.getCoordinates().length;
+                        int pointsToGenerate = length - poinsToIntersect;
+                        Coordinate[] coords;
+                        int order = randomGenerator.nextInt(2); //add point before or after keeped part
+
+                        if (pointsToGenerate == 1) {
+                            PointGenerator pg = new PointGenerator();
+                            pg.setGeometryFactory(geometryFactory);
+                            pg.setBoundingBox(env);
+                            Point pt = (Point) pg.create();                            
+                            //merge all points and create a new linestring
+                            switch (order) {
+                                case 0:
+                                    coords = concatenate(line.getCoordinates(), pt.getCoordinates());
+                                    this.returned = geometryFactory.createLineString(coords);
+                                    break;
+                                case 1:
+                                    coords = concatenate(pt.getCoordinates(), line.getCoordinates());
+                                    this.returned = geometryFactory.createLineString(coords);
+                                    break;
+                            }
+
+                        } else {
+                            //generate the rest of points 
+                            LineStringGenerator pg = new LineStringGenerator();
+                            pg.setGeometryFactory(geometryFactory);
+                            pg.setBoundingBox(env);
+                            //pg.setGenerationAlgorithm(LineStringGenerator.HORZ);
+                            if (pointsToGenerate < 4) {
+                                pg.setGenerationAlgorithm(LineStringGenerator.HORZ);
+                            }
+                            if (pointsToGenerate > 350) {
+                                pointsToGenerate = 350;
+                            }
+                            pg.setNumberPoints(pointsToGenerate);
+                            LineString pt = (LineString) pg.create();
+                            //merge all points and create a new linestring
+                            switch (order) {
+                                case 0:
+                                    coords = concatenate(line.getCoordinates(), pt.getCoordinates());
+                                    this.returned = geometryFactory.createLineString(coords);
+                                    break;
+                                case 1:
+                                    coords = concatenate(pt.getCoordinates(), line.getCoordinates());
+                                    this.returned = geometryFactory.createLineString(coords);
+                                    break;
+                            }
+                        }
                         break;
+
                     case "LinearRing":
                         break;
                     case "MultiLineString":
--- test/com/vividsolutions/jts/geom/create/CreateOverlapsGeometryObjectTest.java
+++ test/com/vividsolutions/jts/geom/create/CreateOverlapsGeometryObjectTest.java
@@ -5,7 +5,15 @@
  */
 package com.vividsolutions.jts.geom.create;
 
+import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
+import com.vividsolutions.jts.io.gml2.LineStringGenerator;
+import java.util.Random;
+import static junit.framework.Assert.assertTrue;
 import junit.framework.TestCase;
 
 /**
@@ -13,16 +21,16 @@ import junit.framework.TestCase;
  * @author jsaveta
  */
 public class CreateOverlapsGeometryObjectTest extends TestCase {
-    
+
     public CreateOverlapsGeometryObjectTest(String testName) {
         super(testName);
     }
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
     }
-    
+
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
@@ -31,14 +39,35 @@ public class CreateOverlapsGeometryObjectTest extends TestCase {
     /**
      * Test of generateGeometry method, of class CreateOverlapsGeometryObject.
      */
-    public void testGenerateGeometry() {
-        System.out.println("generateGeometry");
-        CreateOverlapsGeometryObject instance = null;
-        Geometry expResult = null;
-        Geometry result = instance.generateGeometry();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testGenerateGeometry() throws ParseException {
+        GeometryFactory geometryFactory = new GeometryFactory();
+        WKTReader reader = new WKTReader(geometryFactory);
+        Random rand = new Random();
+        for (int i = 0; i < 1000; i++) {
+            LineStringGenerator pg = new LineStringGenerator();
+            pg.setGeometryFactory(geometryFactory);
+            pg.setBoundingBox(new Envelope(-180, 180, -90, 90));
+
+            //10 to 350 points (check generation of larger linestrings)
+            int numPoints = rand.nextInt(350) + 10;
+            System.out.println("numPoints " + numPoints);
+            pg.setNumberPoints(numPoints);
+
+            LineString line = (LineString) pg.create();
+            CreateOverlapsGeometryObject instanceL1 = new CreateOverlapsGeometryObject(line, GeometryType.GeometryTypes.LineString);
+
+            Geometry resultL1 = instanceL1.generateGeometry();
+            System.out.println("line: " + line + " size " + line.getCoordinates().length);
+            System.out.println("result: " + resultL1 + " size " + resultL1.getCoordinates().length);
+//            System.out.println("intersection " + line.intersection(resultL1));
+
+//            LineString line = (LineString) reader.read("LINESTRING( 18.918457 3.501085, 19.045658 3.531829, 19.164791 3.564040, 19.281864 3.612011, -5.998535 6.521366, 1 1)");
+//            LineString resultL1 = (LineString) reader.read("LINESTRING(-5.998535 6.521366, 18.918457 3.501085, 19.045658 3.531829, 19.164791 3.564040, 19.281864 3.612011, -5.998535 6.521366)");
+//            System.out.println(" inter " + line.intersection(resultL1));
+//            assertTrue(resultL1.overlaps(line));
+            assertTrue(line.overlaps(resultL1));
+
+        }
     }
-    
+
 }
